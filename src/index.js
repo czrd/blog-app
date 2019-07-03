25162efaed27d95d742cde1eeffd4e290889ecd8
Takeshi Itami <tksh.itm@gmail.com>
2019-07-04 06:38:31 +0900

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { createPost, createUser } from './actions';
 import App from './components/App';
-import appReducer from './reducers';
-import DevTools from './containers/DevTools';
-import { compose } from 'redux';
-import { persistState } from 'redux-devtools';
+import configureStore from './store';
 
-function getSessionKey() {
-  const matches = window.location.href.match(/[?&]debug=([^&#]+)\b/);
-  return matches && matches.length > 0 ? matches[1] : null;
-}
-
-const enhancer = compose(
-  DevTools.instrument(),
-  persistState(getSessionKey())
-);
-
-let store = createStore(appReducer, {}, enhancer);
+const store = configureStore();
 const initialState = store.getState();
 
 if (!initialState.users || initialState.users.length === 0) {
