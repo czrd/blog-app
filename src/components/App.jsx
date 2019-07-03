913b6011ab73f16861510308d18127920a3a86df
Takeshi Itami <tksh.itm@gmail.com>
2019-07-03 18:53:59 +0900

    add App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ConnectedFilterList from '../containers/ConnectedFilterList';
+import ConnectedPostList from '../containers/ConnectedPostList';
+
+const App = ({ store }) => (
+  <Provider store={store}>
+    <div>
+      <h1>React/Redux blog</h1>
+      <div>
+        <ConnectedPostList />
+      </div>
+      <div>
+        <ConnectedFilterList />
+      </div>
+    </div>
+  </Provider>
+);
+
+export default App;
