add5b0547108b0f78029547b8adfc8045f20bca1
Takeshi Itami <tksh.itm@gmail.com>
2019-07-04 07:09:22 +0900

    add webpack.config.dev.js

---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+const prodConfig = require('./webpack.config.prod');
+
+module.exports = Object.assign(prodConfig, {
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    prodConfig.entry
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+    contentBase: prodConfig.output.path,
+    publicPath: '/'
+  },
+  plugins: prodConfig.plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ])
+});
