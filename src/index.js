0ac4d689f7712d2c1fd6df571924317530460ccb
Takeshi Itami <tksh.itm@gmail.com>
2019-07-04 06:01:53 +0900

    use persistState

--- src/index.js
+++ src/index.js
@@ -5,8 +5,22 @@ import { createPost, createUser } from './actions';
 import App from './components/App';
 import appReducer from './reducers';
 import DevTools from './containers/DevTools';
+import { compose } from 'redux';
+import { persistState } from 'redux-devtools';
+
+function getSessionKey() {
+  const matches = window.location.href.match(/[?&]debug=([^&#]+)\b/);
+  return matches && matches.length > 0 ? matches[1] : null;
+}
+
+const enhancer = compose(
+  DevTools.instrument(),
+  persistState(getSessionKey())
+);
 
 let store = createStore(appReducer, {}, DevTools.instrument());
+
+const initialState = store.getState();
 store.dispatch(createUser('dan', 'Daniel'));
 store.dispatch(createUser('john', 'John'));
 store.dispatch(
