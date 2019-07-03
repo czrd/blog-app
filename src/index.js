a694946c103d59d1ad943b78b888e6561045bb70
Takeshi Itami <tksh.itm@gmail.com>
2019-07-03 16:03:59 +0900

    modify index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("hello, world!")
+import { createStore } from 'redux';
+import { createPost, createUser } from './actions';
+import appReducer from './reducers';
+
+let store = createStore(appReducer);
+store.subscribe(() => {
+  console.log('state changed: ', store.getState());
+});
+console.log(store.getState());
+
+store.dispatch(createUser('dan', 'Daniel Bugl'));
+store.dispatch(createUser('des', 'Destiny'));
+
+store.dispatch(
+  createPost('dan', {
+    title: 'First post',
+    text: 'Hello world! This is the first blog post.',
+    category: 'welcome'
+  })
+);
+
+store.dispatch(
+  createPost('des', {
+    title: 'Another test',
+    text: 'This is another test blog post.',
+    category: 'test'
+  })
+);
