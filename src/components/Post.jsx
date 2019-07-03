691c66d4d361f4d532e644f429da1b06f07dd1b6
Takeshi Itami <tksh.itm@gmail.com>
2019-07-03 16:32:39 +0900

    add Post.jsx

---
+++ src/components/Post.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import User from './User';
+
+const Post = ({ user, title, text, category, created, updated }) => (
+  <span>
+    <b>{title}</b>
+    <i>
+      {' ~ '}
+      <User {...user} />
+    </i>
+    <br />
+    (Created at: <TimeStamp data={created} />, Updated at: <TimeStamp data={updated} />)
+  </span>
+);
+
+export default Post;
