abef2e4ca4ce1e00d343ed82ec0c926836daacf9
Takeshi Itami <tksh.itm@gmail.com>
2019-07-03 14:23:55 +0900

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js"',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main-[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extenstions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+};
