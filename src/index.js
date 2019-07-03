bd109c68e9a83e89ce2ff442f3f3b56f8e34c524
Takeshi Itami <tksh.itm@gmail.com>
2019-07-03 19:05:07 +0900

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { createPost, createUser } from './actions';
 import App from './components/App';
 import appReducer from './reducers';
 
 let store = createStore(appReducer);
+store.dispatch(createUser('dan', 'Daniel'));
+store.dispatch(createUser('john', 'John'));
+store.dispatch(createPost('dan', { title: 'hello', text: 'hello, world!', category: 'tech' }));
+store.dispatch(createPost('john', { title: "John's post", text: 'hi!', category: 'math' }));
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
