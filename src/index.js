3a465d632b794355c0ae32ad0a5ea5d3e5d1b5fa
Takeshi Itami <tksh.itm@gmail.com>
2019-07-03 19:00:59 +0900

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { createPost, createUser } from './actions';
+import App from './components/App';
 import appReducer from './reducers';
 
 let store = createStore(appReducer);
-store.subscribe(() => {
-  console.log('state changed: ', store.getState());
-});
-console.log(store.getState());
 
-store.dispatch(createUser('dan', 'Daniel Bugl'));
-store.dispatch(createUser('des', 'Destiny'));
-
-store.dispatch(
-  createPost('dan', {
-    title: 'First post',
-    text: 'Hello world! This is the first blog post.',
-    category: 'welcome'
-  })
-);
-
-store.dispatch(
-  createPost('des', {
-    title: 'Another test',
-    text: 'This is another test blog post.',
-    category: 'test'
-  })
-);
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
