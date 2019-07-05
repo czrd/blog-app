9880ceace35a64200489eea9cc208a950b021bc9
Takeshi Itami <tksh.itm@gmail.com>
2019-07-05 15:47:32 +0900

    add fetchUsersByUsernames method

--- src/actions/users.js
+++ src/actions/users.js
@@ -14,3 +14,7 @@ export const fetchUser = username =>
     types: [FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_FAILURE],
     promise: fetch(`http://localhost:8080/api/users/${username}`).then(response => response.json())
   });
+
+export const fetchUsersByUsernames = uernames => dispatch => {
+  Promise.all(usernames.map(username => fetchUser(username)(dispatch)));
+};
