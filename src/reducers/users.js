1baf67819c0e73a4ea5c403b1258d1f750e34980
Takeshi Itami <tksh.itm@gmail.com>
2019-07-05 15:29:22 +0900

    add FETCH_USER_SUCCESS case in user reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,13 @@
-import { CREATE_USER } from '../actionTypes';
+import { CREATE_USER, FETCH_USER_SUCCESS } from '../actionTypes';
 
 export default function usersReducer(state = [], action) {
   const { type, ...user } = action;
   switch (type) {
     case CREATE_USER: {
       return [...state, user];
+    }
+    case FETCH_USER_SUCCESS: {
+      return [...state, action.result];
     }
     default:
       return state;
