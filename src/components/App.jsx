1459060252236f50eb2a92ecbb59b34c7148e2b0
Takeshi Itami <tksh.itm@gmail.com>
2019-07-07 21:31:09 +0900

    edit index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ConnectedFilterList from '../containers/ConnectedFilterList';
 import ConnectedPostList from '../containers/ConnectedPostList';
+import ConnectedLoading from '../containers/ConnectedLoading';
 import DevTools from '../containers/DevTools';
 
 const App = ({ store }) => (
@@ -13,6 +14,9 @@ const App = ({ store }) => (
       </div>
       <div>
         <ConnectedFilterList />
+      </div>
+      <div>
+        <ConnectedLoading />
       </div>
     </div>
     {process.env.NODE_ENV !== 'production' && <DevTools />}
