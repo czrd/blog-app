8ef73d0cca92778f2f5689c9c0ce8e0b2479fa5e
Takeshi Itami <tksh.itm@gmail.com>
2019-07-09 15:03:23 +0900

    send post request

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,25 +1,23 @@
-import {
-  CREATE_USER,
-  FETCH_USER_FAILURE,
-  FETCH_USER_REQUEST,
-  FETCH_USER_SUCCESS
-} from '../actionTypes';
+import { CREATE_USER_FAILURE, CREATE_USER_REQUEST, CREATE_USER_SUCCESS, FETCH_USER_FAILURE, FETCH_USER_REQUEST, FETCH_USER_SUCCESS } from '../actionTypes';
 import { thunkCreator } from './utils';
 
-export const createUser = (username, realname) => {
-  return {
-    type: CREATE_USER,
-    username,
-    realname
-  };
-};
+export const createUser = (username, realname) =>
+  thunkCreator({
+    types: [CREATE_USER_REQUEST, CREATE_USER_SUCCESS, CREATE_USER_FAILURE],
+    promise: fetch('http://localhost:8080/api/users', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, realname })
+    }).then(response => response.json())
+  });
 
 export const fetchUser = username =>
   thunkCreator({
     types: [FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_FAILURE],
-    promise: fetch(`http://localhost:8080/api/users/${username}`).then(
-      response => response.json()
-    )
+    promise: fetch(`http://localhost:8080/api/users/${username}`).then(response => response.json())
   });
 
 export const fetchUsersByUsernames = usernames => dispatch => {
